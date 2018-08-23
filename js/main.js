af50510b7f9e368ad5c9f2dbf20dcc3cf3b9ffb4
Chris <chris211321@gmail.com>
2018-08-23 18:34:27 -0400

    about and demo1

--- js/main.js
+++ js/main.js
@@ -1,5 +1,78 @@
-import Typewriter from '../../type-right/dist/type-right.js'
+import Typewriter from '../../type-write/dist/type-write.js'
 
 document.addEventListener('DOMContentLoaded', () => {
-  
+
+  // ABOUT SECTION
+
+  const about1 = document.querySelector('.about1')
+  const about2 = document.querySelector('.about2')
+  const about3 = document.querySelector('.about3')
+  const about4 = document.querySelector('.about4')
+
+  const aboutTw1 = new Typewriter(about1)
+  const aboutTw2 = new Typewriter(about2)
+  const aboutTw3 = new Typewriter(about3)
+  const aboutTw4 = new Typewriter(about4)
+
+  const clearAll = () => {
+    aboutTw1.clearText()
+    aboutTw2.clearText()
+    aboutTw3.clearText()
+    aboutTw4.clearText()
+
+    aboutTw1.start()
+  }
+
+  aboutTw1
+    .type('Fast')
+    .removeCursor()
+    .rest(300)
+    .then(aboutTw2.start.bind(aboutTw2))
+    .start()
+
+  aboutTw2
+    .type('Easy to use')
+    .removeCursor()
+    .rest(300)
+    .then(aboutTw3.start.bind(aboutTw3))
+
+  aboutTw3
+    .type('Fully-featured')
+    .removeCursor()
+    .rest(300)
+    .then(aboutTw4.start.bind(aboutTw4))
+
+  aboutTw4
+    .type('Dependency-free')
+    .removeCursor()
+    .rest(2000)
+    .then(clearAll)
+
+  // DEMO 1
+
+  const test1 = document.querySelector('.test1')
+
+  const testTw1 = new Typewriter(test1, {
+    loop: true,
+    typeSpeed: 80,
+    deleteSpeed: 80,
+    typeColor: 'red'
+  })
+
+  testTw1
+    .type('You can type')
+    .rest(500)
+    .changeOps({ deleteSpeed: 80 })
+    .remove(4)
+    .type('edit')
+    .rest(500)
+    .remove(4)
+    .type('synchronize callbacks')
+    .rest(500)
+    .changeOps({ deleteSpeed: 20 })
+    .remove(21)
+    .type('change options on the go')
+    .rest(500)
+    .clear()
+    .start()
 })
